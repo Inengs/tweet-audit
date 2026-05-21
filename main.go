ae803712a8b12c1e1db1bed9ddbdd4b231d47bc3
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2026-05-21 15:23:13 +0100

    added tests to archive parser

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/Inengs/tweet-audit/src/archive"
+)
+
 func main() {
+	filepath := os.Args[1]
+
+	tweets, err := archive.FileParser(filepath)
 	
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+
+	for _, tweet := range tweets {
+		fmt.Println(tweet)
+	}
 }
--- src/archive/parser_test.go
+++ src/archive/parser_test.go
@@ -1,6 +1,8 @@
 package archive_test
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/Inengs/tweet-audit/src/archive"
@@ -19,3 +21,52 @@ func TestFileParser(t *testing.T) {
 		t.Errorf("expected 2 tweets, got %d", len(tweets))
 	}
 }
+
+func TestForInvalidPath(t *testing.T) {
+	var filepath = "dummydata"
+
+	_, err := archive.FileParser(filepath)
+
+	if err == nil{
+		t.Errorf("expected an error but got nil")
+	}
+
+	if !errors.Is(err, os.ErrNotExist) {
+    	t.Errorf("expected ErrNotExist but got: %v", err)
+	}
+}
+
+func TestForWrongFormat(t *testing.T) {
+	var filepath = "testdata/invalid.json"
+
+	_, err := archive.FileParser(filepath)
+
+	if err == nil {
+		t.Errorf("got a valid file: %v", err)
+	}
+}
+
+func TestForEmptyArrayFile(t *testing.T) {
+	var filepath = "testdata/empty_array.json"
+
+	tweets, err := archive.FileParser(filepath)
+
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	
+	if len(tweets) != 0 {
+		t.Errorf("expected empty slice, got %d tweets", len(tweets))
+	}
+}
+
+func TestForEmptyFile(t *testing.T) {
+	var filepath = "testdata/empty.json"
+
+	_, err := archive.FileParser(filepath)
+
+	if err == nil {
+		t.Errorf("expected error: %v", err)
+	}
+}
+
